5258247fc2b7b4498dccfe066fedb7fd496aadd6
thapaSanjay <sanjay@codegearthemes.com>
2023-12-29 09:50:26 +0545

    team card widget updated

--- widgets/team-card/team-card.php
+++ widgets/team-card/team-card.php
@@ -17,7 +17,7 @@ class Team_Card extends Widget_Base {
 	public function __construct($data = [], $args = null) {
 
         parent::__construct($data, $args);
-        wp_register_style( 'team-cards', \Designer::plugin_url().'widgets/team-card/assets/css/team.css', array(), '1.0.0', 'all' );
+        wp_register_style( 'team-cards', \Designer::plugin_url().'widgets/team-card/assets/team.css', array(), '1.0.0', 'all' );
 
     }
 
@@ -57,7 +57,7 @@ class Team_Card extends Widget_Base {
 	 * @return string Widget icon.
 	 */
 	public function get_icon() {
-		return 'eicon-image-box';
+		return 'designer-icon eicon-image-box';
 	}
 
     /**
@@ -70,7 +70,7 @@ class Team_Card extends Widget_Base {
 	 * @return string Widget help URL.
 	 */
 	public function get_custom_help_url() {
-		return 'https://wordpress.org/support/plugin/designer/';
+		return 'https://www.codegearthemes.com/contact';
 	}
 
     /**
@@ -144,6 +144,36 @@ class Team_Card extends Widget_Base {
 			]
 		);
 
+		$this->add_control(
+			'show_secondary_image',
+			[
+				'label' => esc_html__( 'Show Secondary Image', 'designer' ),
+				'type' => \Elementor\Controls_Manager::SWITCHER,
+				'label_on' => esc_html__( 'Show', 'designer' ),
+				'label_off' => esc_html__( 'Hide', 'designer' ),
+				'return_value' => 'yes',
+				'default' => 'no',
+			]
+		);
+
+		$this->add_control(
+			'secondary_image',
+			[
+                'label' => __( 'Secondary Image', 'designer' ),
+				'description'	=> __('Replace Image on Hover', 'designer'),
+				'type' => Controls_Manager::MEDIA,
+                'default' => [
+					'url' => Utils::get_placeholder_image_src(),
+				],
+				'dynamic' => [
+					'active' => true,
+				],
+				'condition'	=> [
+					'show_secondary_image'	=> 'yes',
+				]
+			]
+		);
+
 		$this->add_group_control(
 			Group_Control_Image_Size::get_type(),
 			[
@@ -163,6 +193,7 @@ class Team_Card extends Widget_Base {
 				'type' => Controls_Manager::TEXT,
 				'label_block' => true,
 				'label' => __( 'Name', 'designer' ),
+				'default'	=> __('Fred Williams', 'designer'),
 				'placeholder' => __( 'Type title here', 'designer' ),
 				'dynamic' => [
 					'active' => true,
@@ -176,6 +207,7 @@ class Team_Card extends Widget_Base {
 				'type' => Controls_Manager::TEXTAREA,
 				'label_block' => true,
 				'label' => __( 'Position', 'designer' ),
+				'default'	=> __('Xyz Ceo', 'designer'),
 				'placeholder' => __( 'Type position here', 'designer' ),
 				'dynamic' => [
 					'active' => true,
@@ -226,7 +258,6 @@ class Team_Card extends Widget_Base {
 				'label' => __( 'Socail Profile Link', 'designer' ),
 				'type' => Controls_Manager::URL,
 				'label_block' => true,
-				'placeholder' => 'https://twitter.com',
 				'dynamic' => [
 					'active' => true,
 				]
@@ -239,25 +270,32 @@ class Team_Card extends Widget_Base {
 				'show_label' => false,
 				'type' => Controls_Manager::REPEATER,
 				'fields' => $repeater->get_controls(),
-				'title_field' => '<# print(title || "Social Profile"); #>',
-                'default' => array_fill( 0, 1, [
-                    'default' => [
-                        'value' => 'fab fa-twitter',
-                        'library' => 'fa-brands',
-                    ]
-                ])
-			]
-		);
+				'default'	=> [
+					[
+						'title' => __( 'Instagram', 'designer' ),
+						'icon'	=> [
+							'value' => 'fab fa-instagram',
+                    		'library' => 'fa-brands',
+						]
+					],
+					[
+						'title' => __( 'Facebook', 'designer' ),
+						'icon'	=> [
+							'value' => 'fab fa-facebook',
+                    		'library' => 'fa-brands',
+						]
+					],
+					[
+						'title' => __( 'Twitter', 'designer' ),
+						'icon'	=> [
+							'value' => 'fab fa-twitter',
+                    		'library' => 'fa-brands',
+						]
+					],
+					
 
-		$this->add_control(
-			'feature_1',
-			[
-				'label' => esc_html__( 'Need more options', 'designer' ),
-				'type' => \Elementor\Controls_Manager::BUTTON,
-				'separator' => 'before',
-				'label_block' => false,
-				'button_type' => 'danger',
-				'text' => __( '<a style="color: #fff; font-size: 10px; padding: 0 10px; height: 100%; display: block; line-height: 28px;" href="https://codegearthemes.com/products/designer/" target="_blank" >Buy Pro</a>', 'designer' ),
+				],
+				'title_field' => '{{{ title }}}',
 			]
 		);
 
@@ -293,26 +331,28 @@ class Team_Card extends Widget_Base {
 				'label'     => __( 'Layout', 'designer' ),
 				'type'      => Controls_Manager::SELECT,
 				'options'   => [
-					'layout-1'        => __( 'Layout 1', 'designer' ),
-					'layout-2'        => __( 'Layout 2', 'designer' ),
-                    'layout-3'        => __( 'Layout 3', 'designer' ),
-					'layout-4'        => __( 'Layout 4', 'designer' ),
-                    'layout-5'        => __( 'Layout 5', 'designer' ),
-					'layout-6'        => __( 'Layout 6', 'designer' ),
+					'info-below'        		=> __( 'Info Below', 'designer' ),
+					'info-below-left'        	=> __( 'Info Below Left', 'designer' ),
+                    'info-from-bottom'        	=> __( 'Info From Bottom', 'designer' ),
+					'info-on-hover-inset'       => __( 'Info on Hover Inset', 'designer' ),
+                    'info-on-hover'       		=> __( 'Info on Hover', 'designer' ),
+					'social-on-hover'			=>__( 'Socials Icon on Hover', 'designer' ),
+					'social-position-on-hover'  => __( 'Socials Icon and Position on Hover', 'designer' ),
 				],
-				'default'   => 'layout-1',
+				'default'   => 'info-below',
 			]
 		);
 
 		$this->add_control(
-			'feature_2',
+			'image_hover',
 			[
-				'label' => esc_html__( 'Need more options', 'designer' ),
-				'type' => \Elementor\Controls_Manager::BUTTON,
-				'separator' => 'before',
-				'label_block' => false,
-				'button_type' => 'danger',
-				'text' => __( '<a style="color: #fff; font-size: 10px; padding: 0 10px; height: 100%; display: block; line-height: 28px;" href="https://codegearthemes.com/products/designer/" target="_blank" >Buy Pro</a>', 'designer' ),
+				'label'     => __( 'Image Hover', 'designer' ),
+				'type'      => Controls_Manager::SELECT,
+				'options'   => [
+					''        => __( 'None', 'designer' ),
+					'zoom'        => __( 'Zoom', 'designer' ),
+				],
+				'default'   => '',
 			]
 		);
 
@@ -330,10 +370,178 @@ class Team_Card extends Widget_Base {
         );
 
 		$this->add_control(
+			'image_heading',
+			[
+				'label' => esc_html__( 'Image', 'designer' ),
+				'type' => Controls_Manager::HEADING,
+			],
+		);
+
+		$this->add_control(
+			'image_border_radius',
+			[
+				'label' => esc_html__( 'Border radius', 'designer' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', 'em' ],
+				'selectors' => [
+					'{{WRAPPER}}  .block--team-card .team-inner .image-wrapper' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+
+
+		
+		$this->add_control(
+			'general_heading',
+			[
+				'label' => esc_html__( 'Content', 'designer' ),
+				'type' => Controls_Manager::HEADING,
+			],
+		);
+
+		$this->add_control(
+			'text_align',
+			[
+				'label' => esc_html__( 'Alignment', 'designer' ),
+				'type' => Controls_Manager::CHOOSE,
+				'options' => [
+					'left' => [
+						'title' => esc_html__( 'Left', 'designer' ),
+						'icon' => 'eicon-text-align-left',
+					],
+					'center' => [
+						'title' => esc_html__( 'Center', 'designer' ),
+						'icon' => 'eicon-text-align-center',
+					],
+					'right' => [
+						'title' => esc_html__( 'Right', 'designer' ),
+						'icon' => 'eicon-text-align-right',
+					],
+				],
+				'default' => 'center',
+				'toggle' => true,
+				'selectors' => [
+					'{{WRAPPER}} .rte-content' => 'text-align: {{VALUE}};',
+					'{{WRAPPER}} .block--team-card.designer-item-layout--social-position-on-hover .image-wrapper' => 'text-align: {{VALUE}};',
+					'{{WRAPPER}} .block--team-card.designer-item-layout--social-on-hover .image-wrapper' => 'text-align: {{VALUE}};'
+				],
+				'condition'	=> [
+					'layout!'	=> 'info-below-left',
+				]
+			]
+		);
+
+		$this->add_control(
+			'content_vertical_align',
+			[
+				'label' => esc_html__( 'Content Vertical Alignement', 'designer' ),
+				'type' => Controls_Manager::SELECT,
+				'default' => 'center',
+				'options' => [
+					'flex-start' => esc_html__( 'Top', 'designer' ),
+					'center'  => esc_html__( 'Middle', 'designer' ),
+					'flex-end' => esc_html__( 'Bottom', 'designer' ),
+				],
+				'selectors' => [
+					'{{WRAPPER}} .rte-content' => 'justify-content: {{VALUE}};',
+				],
+				'conditions' => [
+					'relation' => 'or',
+					'terms' => [
+						[
+							'name' => 'layout',
+							'operator' => '===',
+							'value'	=> 'info-on-hover-inset',
+						],
+						[
+							'name' => 'layout',
+							'operator' => '===',
+							'value'	=> 'info-on-hover',
+						],
+					],
+				],
+			],
+			
+		);
+
+		$this->add_responsive_control(
+			'content_padding',
+			[
+				'label' => esc_html__( 'Padding', 'designer' ),
+				'type' => Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%', 'em',],
+				'selectors' => [
+					'{{WRAPPER}} .rte-content' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				]
+			]
+		);
+
+		$this->add_control(
+			'content_border_radius',
+			[
+				'label' => esc_html__( 'Border radius', 'designer' ),
+				'type' => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', 'em' ],
+				'selectors' => [
+					'{{WRAPPER}}  .rte-content' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+		$this->add_control(
+            'content_background',
+            [
+                'label' => __( 'Background Color', 'designer' ),
+                'type' => Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .rte-content' => 'background-color: {{VALUE}}',
+                ],
+				'condition'	=> [
+					'layout!'	=> 'info-below-left',
+				]
+            ]
+        );
+
+		$this->add_control(
+			'content_inset',
+			[
+				'label' => esc_html__( 'Content Background Inner Offset', 'designer' ),
+				'type' => Controls_Manager::SLIDER,
+				'size_units' => [ 'px', '%', 'em', ],
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 100,
+						'step' => 1,
+					],
+					'%' => [
+						'min' => 0,
+						'max' => 100,
+					],
+					'em' => [
+						'min' => 0.1,
+						'max' => 10,
+						'step'	=> 0.1,
+					],
+				],
+				'condition'	=> [
+					'layout'	=> 'info-on-hover-inset',
+				],
+				'selectors'  => array(
+					'{{WRAPPER}} .block--team-card.designer-item-layout--info-on-hover-inset  .rte-content' => 'clip-path: inset({{SIZE}}{{UNIT}} {{SIZE}}{{UNIT}} {{SIZE}}{{UNIT}} {{SIZE}}{{UNIT}});',
+				),
+			]
+		);
+
+
+
+		$this->add_control(
 			'name_heading',
 			[
 				'label' => esc_html__( 'Name', 'designer' ),
-				'type' => \Elementor\Controls_Manager::HEADING
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator'	=> 'before'
 			]
 		);
 
@@ -356,6 +564,38 @@ class Team_Card extends Widget_Base {
                 'scheme' => Typography::TYPOGRAPHY_1,
             ]
         );
+
+		$this->add_control(
+			'name_spacing',
+			[
+				'label' => esc_html__( 'Spacing', 'designer' ),
+				'type' => Controls_Manager::SLIDER,
+				'size_units' => [ 'px', '%', 'em', ],
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 100,
+						'step' => 1,
+					],
+					'%' => [
+						'min' => 0,
+						'max' => 100,
+					],
+					'em' => [
+						'min' => 0.1,
+						'max' => 10,
+						'step'	=> 0.1,
+					],
+				],
+				'default' => [
+					'unit' => 'px',
+					'size' => 5,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .block--team-card .name' => 'margin: 0 0 {{SIZE}}{{UNIT}} 0;',
+				],
+			]
+		);
 
 		$this->add_control(
 			'position_heading',
@@ -386,6 +626,38 @@ class Team_Card extends Widget_Base {
             ]
         );
 
+		$this->add_control(
+			'position_spacing',
+			[
+				'label' => esc_html__( 'Spacing', 'designer' ),
+				'type' => Controls_Manager::SLIDER,
+				'size_units' => [ 'px', '%', 'em', ],
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 100,
+						'step' => 1,
+					],
+					'%' => [
+						'min' => 0,
+						'max' => 100,
+					],
+					'em' => [
+						'min' => 0.1,
+						'max' => 10,
+						'step'	=> 0.1,
+					],
+				],
+				'default' => [
+					'unit' => 'px',
+					'size' => 5,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .block--team-card .position' => 'margin: 0 0 {{SIZE}}{{UNIT}} 0;',
+				],
+			]
+		);
+
         $this->add_control(
 			'social_heading',
 			[
@@ -394,6 +666,80 @@ class Team_Card extends Widget_Base {
 				'separator' => 'before'
 			]
 		);
+
+		$this->add_control(
+			'social_spacing',
+			[
+				'label' => esc_html__( 'Bottom Spacing', 'designer' ),
+				'type' => Controls_Manager::SLIDER,
+				'size_units' => [ 'px', '%', 'em', ],
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 100,
+						'step' => 1,
+					],
+					'%' => [
+						'min' => 0,
+						'max' => 100,
+					],
+					'em' => [
+						'min' => 0.1,
+						'max' => 10,
+						'step'	=> 0.1,
+					],
+				],
+				'default' => [
+					'unit' => 'px',
+					'size' => 5,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .block--team-card.designer-item-layout--social-position-on-hover .image-wrapper .profiles' => 'bottom: {{SIZE}}{{UNIT}};',
+					'{{WRAPPER}} .block--team-card.designer-item-layout--social-on-hover .image-wrapper .profiles' => 'bottom: {{SIZE}}{{UNIT}};',
+				],
+				'condition'	=> [
+					'layout'	=> 'social-position-on-hover',
+					'layout'	=> 'social-on-hover'
+				]
+			]
+		);
+
+		$this->add_control(
+			'social_horizontal_spacing',
+			[
+				'label' => esc_html__( 'Horizontal Spacing', 'designer' ),
+				'type' => Controls_Manager::SLIDER,
+				'size_units' => [ 'px', '%', 'em', ],
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 100,
+						'step' => 1,
+					],
+					'%' => [
+						'min' => 0,
+						'max' => 100,
+					],
+					'em' => [
+						'min' => 0.1,
+						'max' => 10,
+						'step'	=> 0.1,
+					],
+				],
+				'default' => [
+					'unit' => 'px',
+					'size' => 20,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .block--team-card.designer-item-layout--social-position-on-hover .image-wrapper .profiles' => 'padding: 0 {{SIZE}}{{UNIT}};',
+					'{{WRAPPER}} .block--team-card.designer-item-layout--social-on-hover .image-wrapper .profiles' => 'padding: 0 {{SIZE}}{{UNIT}};'
+				],
+				'condition'	=> [
+					'layout'	=> 'social-position-on-hover'
+				]
+			]
+		);
+
 
         $this->add_responsive_control(
 			'icon_width',
@@ -442,7 +788,7 @@ class Team_Card extends Widget_Base {
 					'size' => 16,
 				],
 				'selectors' => [
-					'{{WRAPPER}} .block--team-card .social-item .icon-link' => 'font-size: {{SIZE}}{{UNIT}}',
+					'{{WRAPPER}} .block--team-card .social-item .icon-link' => 'width: {{SIZE}}{{UNIT}}',
 				]
 			]
 		);
@@ -485,6 +831,7 @@ class Team_Card extends Widget_Base {
 				'default'=> '#FFFFFF',
                 'selectors' => [
                     '{{WRAPPER}}  .block--team-card .social-item a' => 'color: {{VALUE}}',
+					'{{WRAPPER}}  .block--team-card .social-item a svg path' => 'fill: {{VALUE}}',
                 ],
             ]
         );
@@ -517,7 +864,8 @@ class Team_Card extends Widget_Base {
                 'type' => Controls_Manager::COLOR,
 				'default'=> '#FFFFFF',
                 'selectors' => [
-                    '{{WRAPPER}}  .block--team-card .social-item a' => 'color: {{VALUE}}',
+                    '{{WRAPPER}}  .block--team-card .social-item:hover a' => 'color: {{VALUE}}',
+					'{{WRAPPER}}  .block--team-card .social-item:hover a  svg path' => 'fill: {{VALUE}}',
                 ],
             ]
         );
@@ -538,19 +886,52 @@ class Team_Card extends Widget_Base {
         $this->end_controls_tabs();
 
 		$this->add_control(
-			'feature_3',
+			'icon_spacing',
 			[
-				'label' => esc_html__( 'Need more options', 'designer' ),
-				'type' => \Elementor\Controls_Manager::BUTTON,
-				'separator' => 'before',
-				'label_block' => false,
-				'button_type' => 'danger',
-				'text' => '<a style="color: #fff; font-size: 10px; padding: 0 10px; height: 100%; display: block; line-height: 28px;" href="https://codegearthemes.com/products/designer/" target="_blank" >'.__( "Buy Pro", "designer").'</a>',
+				'label' => esc_html__( 'Icon Spacing', 'designer' ),
+				'type' => Controls_Manager::SLIDER,
+				'size_units' => [ 'px', '%', 'em', ],
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 100,
+						'step' => 1,
+					],
+					'%' => [
+						'min' => 0,
+						'max' => 100,
+					],
+					'em' => [
+						'min' => 0.1,
+						'max' => 10,
+						'step'	=> 0.1,
+					],
+				],
+				'default' => [
+					'unit' => 'px',
+					'size' => 12,
+				],
+				'selectors' => [
+					'{{WRAPPER}} .block--team-card .profiles li:not(last-of-type)' => 'margin-right: {{SIZE}}{{UNIT}};',
+				],
 			]
 		);
 
         $this->end_controls_section();
     }
+
+	public function render_holder_classes( $settings ){
+		$classes = array();
+
+		$classes[] = 'block--team-card';
+		$classes[] = !empty($settings['layout'] ) ? 'designer-item-layout--'.$settings['layout'] : '';
+		$classes[]	= !empty($settings['image_hover']) ? 'designer-image--hover-'. $settings['image_hover'] : '';
+		$classes[]	= 'yes' === $settings['show_secondary_image'] ? 'designer-secondary-image-active' : '';
+
+		return implode(' ', $classes);
+	}
+
+
 
     /**
 	 * Render widget output on the frontend.
@@ -562,8 +943,13 @@ class Team_Card extends Widget_Base {
 	 */
 	protected function render() {
 
-		$settings = $this->get_settings_for_display();
-		require \Designer::plugin_dir().'widgets/team-card/snippets/'.$settings['layout'].'.php';
+		$settings = $this->get_settings_for_display();?>
+			<div class="<?php echo $this->render_holder_classes($settings); ?>">
+				<?php require \Designer::plugin_dir().'widgets/team-card/snippets/'.$settings['layout'].'.php'; ?>
+			</div>
+
+		<?php
+		
 	}
 
 	/**
